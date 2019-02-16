05e3d30d097cb012a1d33b13fb47ca3c06094caa
szapp <dev.szapp@gmail.com>
2019-02-16 14:43:08 +0100

    Add build script with timestamp

---
+++ build.ps1
@@ -0,0 +1,14 @@
+$srcfile = Resolve-Path "IKLG.VM"
+
+& "C:\Games\Gothic2\_work\Tools\VDFS\GothicVDFS.exe" -b $srcfile | Out-Null
+
+$binfile = Resolve-Path "IKLG.DATA"
+
+$addr = 0x118
+$bytes = [System.IO.File]::ReadAllBytes($binfile);
+$bytes[$addr] = 0x7D;
+$bytes[$addr+1] = 0xBF;
+$bytes[$addr+2] = 0x9F;
+$bytes[$addr+3] = 0x7F;
+
+[System.IO.File]::WriteAllBytes($binfile, $bytes);
