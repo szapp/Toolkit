0353944e6355c0b1608ff8fa9a5d79a2b8d8c630
szapp <dev.szapp@gmail.com>
2020-10-11 14:06:59 +0200

    Add divider symbol G1 and constants

--- Ninja/_intern/Ikarus/Ikarus_Const_G2.d
+++ Ninja/_intern/Ikarus/Ikarus_Const_G2.d
@@ -1,6 +1,27 @@
 // Divider symbol to obtain range of possibly added Ikarus symbols parsed with Ninja
 const int _Ikarus_Ninja_Start = 0;
 
+// Necessary constants
+const int DAM_INDEX_MAX     = 8;
+const int PROT_INDEX_MAX    = DAM_INDEX_MAX;
+const int ITM_TEXT_MAX      = 6;
+const int ATR_HITPOINTS     = 0;
+const int ATR_HITPOINTS_MAX = 1;
+const int ATR_MANA          = 2;
+const int ATR_MANA_MAX      = 3;
+const int PERC_ASSESSDAMAGE = 8;
+const int ITEM_KAT_NF       = 1 << 1;
+const int ITEM_KAT_FF       = 1 << 2;
+const int TRUE              = 1;
+const int FALSE             = 0;
+const int LOOP_CONTINUE     = 0;
+const int LOOP_END          = 1;
+const int ATT_FRIENDLY      = 3;
+const int ATT_NEUTRAL       = 2;
+const int ATT_ANGRY         = 1;
+const int ATT_HOSTILE       = 0;
+// ---
+
 const int GOTHIC_BASE_VERSION = 2;
 
 //--------------------------------------
